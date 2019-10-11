307858c3d91dfb03a7046aae4969c8c08ef9e46e
DanielCarpov <dcarpov@endava.com>
2019-10-11 09:51:03 +0300

    Somer tests added

---
+++ Diploma.Tests/UnitTests/MainMenuTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using System.IO;
+using Diploma.Models;
+using System.Collections.Generic;
+using Diploma.DataProcessing;
+using NSubstitute;
+
+namespace Diploma.Tests.UnitTests
+{
+    public class MainMenuTests
+    {
+        private readonly IConsoleWrapper consoleWrapper;
+        private readonly IConstants constants;
+        private readonly IStudentDataProcessor loader;
+
+        [Fact]
+        public void MainMenuBuilder()
+        {
+            this.consoleWrapper = Substitute.For<IConsoleWrapper>();
+            var constants = Substitute.For<IConstants>();
+            var suboader = Substitute.For<IStudentDataProcessor>();
+        }
+
+    }
+}
---
+++ Diploma.Tests/UnitTests/ValidatorTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Diploma.Models;
+using Diploma.Validators;
+using Xunit;
+using FluentAssertions;
+
+namespace Diploma.Tests.UnitTests
+{
+    public class ValidatorTests
+    {
+        [Fact]
+        public void Validator_NullStudentModel_False()
+        {
+            var student = new StudentModel();
+
+            Validator validator = new Validator();
+
+            var act = validator.ValidateStudentRecord(student);
+
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validator_ValidStudentModel_True()
+        {
+            var student = new StudentModel();
+            student.LastName = "petrov";
+            student.FirstName = "peteya";
+            List<Subject> Grades = new List<Subject>();
+            Subject sbj = new Subject();
+            sbj.SubjectName = "lang";
+            sbj.Grade = 10;
+            Grades.Add(sbj);          
+            student.Grades = Grades;
+
+            Validator validator = new Validator();
+
+            var act = validator.ValidateStudentRecord(student);
+
+            act.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validator_GradeNotInRange_False()
+        {
+            var student = new StudentModel();
+            student.LastName = "petrov";
+            student.FirstName = "peteya";
+            List<Subject> Grades = new List<Subject>();
+            Subject sbj = new Subject();
+            sbj.SubjectName = "lang";
+            sbj.Grade = -10;
+            Grades.Add(sbj);
+            student.Grades = Grades;
+
+            Validator validator = new Validator();
+
+            var act = validator.ValidateStudentRecord(student);
+
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validator_NullFirstName_False()
+        {
+            var student = new StudentModel();
+            student.LastName = "petrov";
+            student.FirstName = null;
+            List<Subject> Grades = new List<Subject>();
+            Subject sbj = new Subject();
+            sbj.SubjectName = "lang";
+            sbj.Grade = 10;
+            Grades.Add(sbj);
+            student.Grades = Grades;
+
+            Validator validator = new Validator();
+
+            var act = validator.ValidateStudentRecord(student);
+
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validator_NullLastName_False()
+        {
+            var student = new StudentModel();
+            student.LastName = null;
+            student.FirstName = "peteya";
+            List<Subject> Grades = new List<Subject>();
+            Subject sbj = new Subject();
+            sbj.SubjectName = "lang";
+            sbj.Grade = 10;
+            Grades.Add(sbj);
+            student.Grades = Grades;
+
+            Validator validator = new Validator();
+
+            var act = validator.ValidateStudentRecord(student);
+
+            act.Should().BeFalse();
+        }
+    }
+}
